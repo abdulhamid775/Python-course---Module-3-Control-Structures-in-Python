38f0c95fcfbd8d2c4b1e662e6b345bcb8be8db97
abdulhamid775 <abdulhamid1106s@gmail.com>
2025-07-13 01:20:01 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+# even_or_odd.py
+
+# Taking input from the user
+number = int(input("Enter a number: "))
+
+# Checking even or odd
+if number % 2 == 0:
+    print(f"\n{number} is an even number.")
+else:
+    print(f"\n{number} is an odd number.")
